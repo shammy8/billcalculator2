4f8add544cb4337273abb9c88ee79cfe5e5d2764
Allan Sham <allansham@hotmail.com>
2021-06-13 00:47:50 +0100

    Add method to add a new item to a bill document

--- src/app/bill.service.ts
+++ src/app/bill.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Bill, Item, NewItemWithBill } from './model/bill.model';
+import * as firebase from 'firebase/app';
+// import 'firebase/firestore';
+import { Bill, NewItemWithBill } from './model/bill.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +12,8 @@ export class BillService {
 
   addItem(newItemWithBill: NewItemWithBill) {
     console.log(newItemWithBill);
+    const arrayUnion = firebase.default.firestore.FieldValue.arrayUnion; // TODO why do we need default, other examples don't have it
+    const doc = this.store.doc<Bill>(`bills/${newItemWithBill.bill.uid}`);
+    doc.update({ items: arrayUnion(newItemWithBill.newItem) as any }); // TODO can't get it to work without the as any
   }
 }
