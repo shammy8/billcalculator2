6382beec7111ed0f50f7fa10a40f6043887ed0b4
Allan Sham <allansham@hotmail.com>
2021-06-14 10:25:33 +0100

    Import firebsae properly

--- src/app/bill.service.ts
+++ src/app/bill.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase/app';
-// import 'firebase/firestore';
+import firebase from 'firebase/app';
 import { Bill, Item, NewItemWithBill } from './model/bill.model';
 
 @Injectable({
@@ -12,7 +11,7 @@ export class BillService {
 
   addItem(newItemWithBill: NewItemWithBill) {
     console.log(newItemWithBill);
-    const arrayUnion = firebase.default.firestore.FieldValue.arrayUnion; // TODO why do we need default, other examples don't have it
+    const arrayUnion = firebase.firestore.FieldValue.arrayUnion;
     const doc = this.store.doc<Bill>(`bills/${newItemWithBill.bill.uid}`);
     doc.update({ items: arrayUnion(newItemWithBill.newItem) as any }); // TODO can't get it to work without the as any
   }
