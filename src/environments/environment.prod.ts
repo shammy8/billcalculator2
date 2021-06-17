a5cf40e9619ba6e9800b0219d1ba0a09b48971d2
Allan Sham <allansham@hotmail.com>
2021-06-18 00:51:00 +0100

    Add angular fire set up hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAKULRE-nJF2WpDGr-RFxYNmHQAnXx675I',
+    authDomain: 'bill-calculator-eaac9.firebaseapp.com',
+    databaseURL: 'https://bill-calculator-eaac9.firebaseio.com',
+    projectId: 'bill-calculator-eaac9',
+    storageBucket: 'bill-calculator-eaac9.appspot.com',
+    messagingSenderId: '417329005138',
+    appId: '1:417329005138:web:6ae288a575f943f20ad58b',
+    measurementId: 'G-DE0KR5LFN7',
+  },
 };
