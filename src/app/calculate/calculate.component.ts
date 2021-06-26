57dc952049aee4b7cede45ad7aeea4fb17a455d1
Allan Sham <allansham@hotmail.com>
2021-06-27 00:29:54 +0100

    Fix calculate component

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-// import { Bill, Items, Ledger } from '../model/bill.model';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { /* Bill ,*/ Item, Ledger } from '../model/bill.model';
 
 @Component({
   selector: 'bc-calculate',
   template: `
-    <!-- <div *ngFor="let user1 of ledger | keyvalue">
+    <div *ngFor="let user1 of ledger | keyvalue">
       <div *ngFor="let user2 of user1.value | keyvalue">
         <ng-container *ngIf="user2.value < 0">
           {{ user1.key | titlecase }} owes
@@ -12,44 +17,45 @@ import { Component, Input, OnInit } from '@angular/core';
           £{{ user2.value * -1 | number: '1.2-2' }}
         </ng-container>
       </div>
-    </div> -->
+    </div>
   `,
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalculateComponent implements OnInit {
-  // ledger: Ledger | null = null;
-  // @Input() bill!: Bill;
-  // constructor() {}
+  ledger: Ledger | null = null;
+  @Input() bill!: any; // TODO
+
   ngOnInit(): void {
-    // this.ledger = this.calculate(this.bill.items);
+    this.ledger = this.calculate(this.bill.items);
   }
-  // calculate(items: Items) {
-  //   const ledger: Ledger = this.createObjectWithEveryone(
-  //     this.createObjectWithEveryone<number>(0)
-  //   );
-  //   for (const item of Object.values(items)) {
-  //     for (const sharedBy of Object.values(item.sharedBy)) {
-  //       ledger[item.paidBy][sharedBy.user] +=
-  //         sharedBy.settled === true
-  //           ? 0
-  //           : item.cost / Object.keys(item.sharedBy).length;
-  //       ledger[sharedBy.user][item.paidBy] -=
-  //         sharedBy.settled === true
-  //           ? 0
-  //           : item.cost / Object.keys(item.sharedBy).length;
-  //     }
-  //   }
-  //   return ledger;
-  // }
-  // private createObjectWithEveryone<T>(value: T): { [key: string]: T } {
-  //   const object: { [key: string]: T } = {};
-  //   this.bill.usersArray.forEach((person) => {
-  //     if (typeof value === 'object' && value !== null) {
-  //       object[person] = { ...value };
-  //     } else {
-  //       object[person] = value;
-  //     }
-  //   });
-  //   return object;
-  // }
+
+  calculate(items: Item[]) {
+    const ledger: Ledger = this.createObjectWithEveryone(
+      this.createObjectWithEveryone<number>(0)
+    );
+
+    for (const item of items) {
+      for (const sharedBy of item.sharedBy) {
+        console.log(item.paidBy, sharedBy.friend);
+        ledger[item.paidBy][sharedBy.friend] +=
+          sharedBy.settled === true ? 0 : item.cost / item.sharedBy.length;
+        ledger[sharedBy.friend][item.paidBy] -=
+          sharedBy.settled === true ? 0 : item.cost / item.sharedBy.length;
+      }
+    }
+    return ledger;
+  }
+
+  private createObjectWithEveryone<T>(value: T): { [key: string]: T } {
+    const object: { [key: string]: T } = {};
+    this.bill.friends.forEach((person: any) => {
+      if (typeof value === 'object' && value !== null) {
+        object[person] = { ...value };
+      } else {
+        object[person] = value;
+      }
+    });
+    return object;
+  }
 }
