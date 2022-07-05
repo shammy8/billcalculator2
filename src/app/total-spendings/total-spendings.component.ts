ba12d4acd0f6a0ab4d1c8581cf660c84cd989f87
Allan Sham <allansham@hotmail.com>
2022-07-05 17:47:41 +0100

    add grand total

--- src/app/total-spendings/total-spendings.component.ts
+++ src/app/total-spendings/total-spendings.component.ts
@@ -1,25 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { BillWithItems, ItemWithId } from '../model/bill.model';
-import { createObjectWithEveryone } from '../utility-functions';
+import { Component, Input, OnInit } from "@angular/core";
+import { BillWithItems, ItemWithId } from "../model/bill.model";
+import { createObjectWithEveryone } from "../utility-functions";
 
 @Component({
-  selector: 'bc-total-spendings',
+  selector: "bc-total-spendings",
   template: `<div *ngFor="let friend of totalSpendings | keyvalue">
-    {{ friend.key | titlecase }} spent £{{ friend.value | number: '1.2-2' }}
-  </div>`,
+      {{ friend.key | titlecase }} spent £{{ friend.value | number: "1.2-2" }}
+    </div>
+    <br />
+    Grand total: <b>£{{ grandTotal | number: "1.2-2" }}</b>`,
   styles: [],
 })
 export class TotalSpendingsComponent implements OnInit {
   totalSpendings: { [key: string]: number } = {};
+  grandTotal = 0;
 
   @Input() billWithItems!: BillWithItems;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.totalSpendings = this.calculateTotalSpendings(
-      this.billWithItems.items
-    );
+    ({
+      totalSpendingsObject: this.totalSpendings,
+      grandTotal: this.grandTotal,
+    } = this.calculateTotalSpendings(this.billWithItems.items));
   }
 
   calculateTotalSpendings(items: ItemWithId[]) {
@@ -27,12 +31,15 @@ export class TotalSpendingsComponent implements OnInit {
       0,
       this.billWithItems.friends
     );
+    let grandTotal = 0;
+
     for (const item of items) {
       for (const sharedBy of item.sharedBy) {
         totalSpendingsObject[sharedBy.friend] +=
           item.cost / Object.keys(item.sharedBy).length;
       }
+      grandTotal += item.cost;
     }
-    return totalSpendingsObject;
+    return { totalSpendingsObject, grandTotal };
   }
 }
